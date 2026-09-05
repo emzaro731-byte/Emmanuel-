9de1903478b9d9aca3089bccd38406c26e96e69b
emzaro731-byte <emzaro731@gmail.com>
2026-09-05 16:36:04 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import { registerRootComponent } from "expo";
+import {AppRegistry} from 'react-native';
 
-import App from "./App";
+import App from './src/App';
 
-registerRootComponent(App);
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
