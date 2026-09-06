03e2cd856c834ed5e0f699f6d70c0e6973f52e01
emzaro731-byte <emzaro731@gmail.com>
2026-09-06 08:15:44 +0100

    Stabilize Destiny AI Android startup activity

--- android/app/src/main/java/com/destinyai/MainActivity.kt
+++ android/app/src/main/java/com/destinyai/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.destinyai
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -7,6 +8,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 class MainActivity : ReactActivity() {
 
     override fun getMainComponentName(): String = "DestinyAI"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(
