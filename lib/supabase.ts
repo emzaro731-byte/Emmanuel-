57751b1f08e0d25d24495b848a092595769023c8
emzaro731-byte <emzaro731@gmail.com>
2026-09-06 05:46:10 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://vihbsfrwnslnmheowkhy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_j8gV4-PeFte1RMgl759uQQ_KrM_3vzK";
+
+const SUPABASE_ANON_KEY =
+  "sb_publishable_j8gV4-PeFte1RMgl759uQQ_KrM_3vzK";
 
 export const supabase = createClient(
   SUPABASE_URL,
@@ -14,7 +16,7 @@ export const supabase = createClient(
       storage: AsyncStorage,
       autoRefreshToken: true,
       persistSession: true,
-      detectSessionInUrl: false
-    }
+      detectSessionInUrl: false,
+    },
   }
 );
