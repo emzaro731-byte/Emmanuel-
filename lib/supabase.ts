06036321f56fbd53c376aa9fcfcfc09be29763e3
emzaro731-byte <emzaro731@gmail.com>
2026-09-06 05:51:43 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,8 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://vihbsfrwnslnmheowkhy.supabase.co";
+const SUPABASE_URL =
+  "https://vihbsfrwnslnmheowkhy.supabase.co";
 
 const SUPABASE_ANON_KEY =
   "sb_publishable_j8gV4-PeFte1RMgl759uQQ_KrM_3vzK";
